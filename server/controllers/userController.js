59b5a2cd2d292e6072432387080522ad449e9aca
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2025-08-24 23:10:25 +0530

    Cipher-Chat

---
+++ server/controllers/userController.js
@@ -0,0 +1,43 @@
+import { generateToken } from "../lib/Utils.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req , res) => {
+    const {fullName , email , password} = req.body;
+
+    try {
+        if(!fullName || !email || !password || !bio) {
+            return res.json({success : false , message : "Missing Details"});
+        }
+        const user = await User.findOne({email});
+
+        if(user) {
+            return res.json({success : false , message : "Account alrrady exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({
+            fullName , email , password : hashedPassword  , bio 
+        })
+
+        const token = generateToken(newUser._id)
+
+        res.json({success : true , userData : newUser , token , message : "Account created successfully"})
+       
+    } catch (error) {
+        console.log(error , message);
+        res.json({success : false , message : error.message})
+    }
+}
+
+export const login = async (req , res) => {
+    try {
+        const {email , password} = req.body;
+        const userData = await User.findOne({email});
+
+
+    } catch (error) {
+
+    }
+}
